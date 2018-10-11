89bd89b1ad24f208d1eca8c3b2b739754de77e0b
Oleh Chemerskyi <ochemerskyi@n-ix.com>
2018-10-11 12:40:18 +0300

    Webpack, babel setup

---
+++ frontend/webpack.js
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-cheap-module-source-map",
+
+  entry: {
+    app: ["./src/index.jsx"]
+  },
+
+  module: {
+    rules: [{ test: /.jsx?$/, use: "babel-loader", exclude: /node_modules/ }]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+
+  output: {
+    filename: "[name].bundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/"
+  },
+
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+
+  devServer: {
+    quiet: false,
+    port: 3000,
+    historyApiFallback: true,
+    contentBase: "./dist",
+    host: "0.0.0.0"
+  }
+};
