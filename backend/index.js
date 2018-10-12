db13f23b308338601aa5e80455db0bf723196b12
Oleh Chemerskyi <ochemerskyi@n-ix.com>
2018-10-12 14:17:19 +0300

    Enable CORS

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,17 @@ const PORT = 4000;
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 app.get("/phones", (req, res) => {
   res.json(phonesData);
 });
